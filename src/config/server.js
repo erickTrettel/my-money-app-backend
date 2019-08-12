7ad7146f7f4b13346cfedab665720eb1956fdfbf
Erick Trettel de Almeida <ericktrettel@gmail.com>
2019-08-12 15:37:03 -0300

    Habilitando o CORS

--- src/config/server.js
+++ src/config/server.js
@@ -4,9 +4,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
+const allowCors = require('./cors')
+
 // middlewares
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
   console.log(`Server running on port ${port}`)

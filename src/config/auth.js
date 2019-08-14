f83b6a0c23e6c8fb134a67946d3f0e93812d8ec5
Erick Trettel de Almeida <ericktrettel@gmail.com>
2019-08-14 13:45:03 -0300

    Corrigindo autenticacao

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,28 +1,24 @@
-const jwt = require('jsonwebtoken')
-const env = require('../.env')
-
+const jwt = require("jsonwebtoken");
+const env = require("../.env");
 module.exports = (req, res, next) => {
   // CORS preflight request
-  if(req.method === 'OPTIONS') {
-    next()
+  if (req.method === "OPTIONS") {
+    next();
   } else {
-    const token = req.body.token || req.query.token || req.headers['authorization']
-
-    if(!token) {
-      return res.status(403).send({ errors: ['No token provided.'] })
+    const token =
+      req.body.token || req.query.token || req.headers["authorization"];
+    if (!token) {
+      return res.status(403).send({ errors: ["No token provided."] });
     }
-
-    jwt,verify(token, env.authSecret, function(err, decoded) {
-      if(err) {
+    jwt.verify(token, env.authSecret, function(err, decoded) {
+      if (err) {
         return res.status(403).send({
-          errors: ['Failed to authenticate token.']
-        })
+          errors: ["Failed to authenticate token."]
+        });
       } else {
-        // Passar o token decodificado para a requisição
-        // req.decoded = decoded
-        next()
+        req.decoded = decoded;
+        next();
       }
-    })
-
+    });
   }
-}
+};
